18d489f0b6ceaf34390902138fa430b2315e8ff4
evstarostin89 <evstarostin89@yandex-team.ru>
2019-05-18 21:52:35 +0300

    Updates

---
+++ precache-manifest.3418eac9a19118044e5cfb293489197b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f1f9ad48b8a3df6baab238bce9b0c74",
+    "url": "/react_tutorial/index.html"
+  },
+  {
+    "revision": "539af4a10a57472cd1b8",
+    "url": "/react_tutorial/static/css/main.2551e264.chunk.css"
+  },
+  {
+    "revision": "9a5c0cc5b7b5942488d9",
+    "url": "/react_tutorial/static/js/2.56cb80cd.chunk.js"
+  },
+  {
+    "revision": "539af4a10a57472cd1b8",
+    "url": "/react_tutorial/static/js/main.39702ddc.chunk.js"
+  },
+  {
+    "revision": "263b86900b78269fabd7",
+    "url": "/react_tutorial/static/js/runtime~main.46666d96.js"
+  }
+]);
